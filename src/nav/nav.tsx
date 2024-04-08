611fc294474fee5eef534da496a6a7a43836e4f8
MJ <chenmengchieh2012@gmail.com>
2024-04-09 02:36:54 +0800

    bar rwd

--- src/nav/nav.tsx
+++ src/nav/nav.tsx
@@ -1,14 +1,21 @@
 import { Link, LinkProps, Outlet, useMatch, useResolvedPath } from "react-router-dom"
 import styles from "../App.module.css"
+import { IoMenu } from 'react-icons/io5';
+import { useEffect, useState } from "react";
+
 const CNav = ()=>{
+    const [show, setShow] = useState(false)
     return <>
     <div className={`${styles["container"]}`}>
-        <div className={`${styles["left"]}`}>
+        {!show && <div onClick={()=>{ setShow(s=>!s) }} className={`${styles["absolute-menuicon"]}`}><IoMenu size={32}/></div>}
+        {show && <div className={`${styles["left"]}`}>
+            <div onClick={()=>{ setShow(s=>!s) }} className={`${styles["menuicon"]}`}><IoMenu size={32}/></div>
             <CCustomLink to="/">首頁</CCustomLink>
             <CCustomLink to="/usage">使用說明</CCustomLink>
             <CCustomLink to="/develop">開發使用</CCustomLink>
         </div>
-        <div className={`${styles["right"]}`}>
+        }
+        <div className={`${styles["right"]} ${!show && styles["hidden"]}`}>
             <Outlet />
         </div>
     </div>
